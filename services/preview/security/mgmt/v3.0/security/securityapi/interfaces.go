06981a350efc8fad44cefc0a163cf6bb8a4fbd6e
licht <tal@wiz.io>
2021-12-17 22:30:17 +0200

    Trying to fix a dependency issue

--- services/preview/security/mgmt/v3.0/security/securityapi/interfaces.go
+++ services/preview/security/mgmt/v3.0/security/securityapi/interfaces.go
@@ -8,7 +8,7 @@ package securityapi
 
 import (
 	"context"
-	"github.com/wiz-sec/azure-sdk-for-go/services/preview/security/mgmt/v3.0/security"
+	"github.com/Azure/azure-sdk-for-go/services/preview/security/mgmt/v3.0/security"
 	"github.com/Azure/go-autorest/autorest"
 )
 
